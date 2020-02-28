de47f91721848bcb90d7559da62c3eafba707143
Hinrik Gylfason <hinrik@heima.land>
2020-02-28 15:08:28 +0000

    Pick lunch event according to a new scoring system

--- lunch-alert.py
+++ lunch-alert.py
@@ -1,4 +1,4 @@
-from datetime import date, datetime
+from datetime import datetime, time
 import logging
 import os
 import re
@@ -16,9 +16,14 @@ LUNCH_CALENDAR_URL = os.environ["LUNCH_CALENDAR_URL"]
 HOLIDAY_CALENDAR_URL = os.environ["HOLIDAY_CALENDAR_URL"]
 SLACK_URL = os.environ["SLACK_URL"]
 
+DATETIME_OVERRIDE = os.environ.get("DATETIME_OVERRIDE", None)
+
 HOLIDAY_MESSAGE_TEMPLATE = ":parrot: Happy {}!"
 
 LUNCH_MESSAGE_TEMPLATE = "<!here> It's lunchtime! Today we're having {}"
+UNSURE_LUNCH_MESSAGE_TEMPLATE = (
+    "<!here> It's lunchtime! Today we're having either {} or {}"
+)
 
 UNKNOWN_LUNCH_MESSAGE = (
     "<!here> It's lunchtime! But unfortunately I can't read the menu for you today "
@@ -40,46 +45,78 @@ if TIMBER_API_KEY and TIMBER_SOURCE_ID:
 def main():
     logger.info("starting")
 
+    if DATETIME_OVERRIDE:
+        logger.info("pretending that today is {}".format(today()))
+
     if is_the_weekend():
         logger.info("stopping - it's the weekend")
         return
 
     holiday_event = fetch_holiday_event()
-    lunch_event = fetch_lunch_event()
+    lunch_events = fetch_lunch_events()
     if holiday_event:
         send_holiday_message(holiday_event)
-    elif lunch_event:
-        send_lunch_message(lunch_event)
-    else:
-        logger.info("no event found - sending")
+    elif lunch_events is None:
         send_unknown_lunch_message()
+    elif len(lunch_events) == 1:
+        send_lunch_message(lunch_events[0])
+    elif len(lunch_events) > 1:
+        send_unsure_lunch_message(lunch_events[0], lunch_events[1])
 
 
 def is_the_weekend():
-    return datetime.utcnow().isoweekday() >= 6
+    return now().isoweekday() >= 6
 
 
-def fetch_lunch_event():
+def fetch_lunch_events():
     events = fetch_events_today(LUNCH_CALENDAR_URL)
 
-    # Filter out the breakfast entry which usually has the summary "1. <description>"
-    events = list(filter(lambda e: not e.summary.startswith("1."), events))
+    # The menu calendar is notoriously unreliable in terms of event data accuracy.
+    # Sometimes the events are all day events, sometimes they have a start time,
+    # sometimes the start time is incorrect. Sometimes the event summary is numbered
+    # so that breakfast is "1." and lunch is "2.", sometimes this numbering is reversed
+    # or repeated.
 
-    # The lunch summary is usually of the form "2. <description>"
+    # We deal with this inaccuracy by scoring the events according to couple of very
+    # simple rules and then we select the event with the highest score. Here is an
+    # example of the scoring:
+
+    # 12:00    2. summary   4
+    # 12:00    summary      3
+    # 12:00    1. summary   2
+    # all day  2. summary   1
+    # all day  summary      0
+    # all day  1. summary  -1
+    # 8:00     2. summary  -2
+    # 8:00     summary     -3
+    # 8:00     1. summary  -4
+
+    scored_events = []
     for event in events:
+        score = 0
+
+        if starts_around_lunch(event):
+            score += 3
+        elif not event.all_day:
+            score -= 3
+
         if event.summary.startswith("2."):
-            return event
+            score += 1
+        elif event.summary.startswith("1."):
+            score -= 1
 
-    # Otherwise let's sort the menu by the the absolute delta time from now and use the
-    # event that's closest
-    events = sorted(events, key=lambda e: delta_from_now(e.start))
-    if events:
-        return events[0]
+        logger.info("event {} got score {}".format(event, score))
+        scored_events.append((score, event))
+
+    sorted_events = sorted(scored_events, reverse=True)
+    if len(sorted_events) > 1 and sorted_events[0][0] == sorted_events[1][0]:
+        return [event for _, event in sorted_events[:2]]
+    elif sorted_events:
+        return [sorted_events[0][1]]
 
 
-def delta_from_now(dt):
-    now = datetime.utcnow()
-    return abs(now.astimezone(UTC) - dt.astimezone(UTC))
+def starts_around_lunch(event):
+    return not event.all_day and today_at(11) < event.start < today_at(13)
 
 
 def fetch_holiday_event():
@@ -95,8 +132,8 @@ def fetch_events_today(url):
     logger.info("fetch_events_today %s", url)
     events = [
         event
-        for event in _fetch_events_with_retry(url, start=date.today())
-        if event.start.date() == date.today()
+        for event in _fetch_events_with_retry(url, start=today())
+        if event.start.date() == today()
     ]
     logger.info("found %s events:", len(events))
     for i, event in enumerate(events):
@@ -131,21 +168,44 @@ def send_lunch_message(event):
     send_message(text)
 
 
+def send_unsure_lunch_message(event_a, event_b):
+    logger.info("send_unsure_lunch_message %s %s", event_a, event_b)
+    summary_a = get_lunch_summary(event_a)
+    summary_b = get_lunch_summary(event_b)
+    text = UNSURE_LUNCH_MESSAGE_TEMPLATE.format(summary_a, summary_b)
+    send_message(text)
+
+
 def get_lunch_summary(event):
     summary = event.summary
-    match = re.match(r"(?:\d+\.?)?(?P<summary>.*)", summary)
+    match = re.match(r"(?:\d+\.? *)?(?P<summary>.*)", summary)
     if match:
         summary = match.group("summary").strip()
     return summary
 
 
 def send_unknown_lunch_message():
+    logger.info("send_unknown_lunch_message")
     send_message(UNKNOWN_LUNCH_MESSAGE)
 
 
 def send_message(text):
     logger.info("send_message %s", text)
     requests.post(SLACK_URL, json={"text": text})
+
+
+def now():
+    if DATETIME_OVERRIDE:
+        return datetime.strptime(DATETIME_OVERRIDE, "%Y-%m-%d")
+    return datetime.utcnow()
+
+
+def today():
+    return now().date()
+
+
+def today_at(hour):
+    return datetime.combine(today(), time(hour=hour, tzinfo=UTC))
 
 
 if __name__ == "__main__":
